54f650c8741da3e2ae4681f5825e62616ff851c8
SYN7777777 <redtrikeygg@gmai.com>
2025-06-02 13:05:01 +0530

    dfzdf

--- server.js
+++ server.js
@@ -8,10 +8,19 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+const allowedOrigins = process.env.FRONTEND_URLS.split(',');
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
