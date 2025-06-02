344d161de110cb4373d4f6fd3601a0fcda9144bc
SYN7777777 <redtrikeygg@gmai.com>
2025-06-02 19:16:33 +0530

    erfsdf

--- server.js
+++ server.js
@@ -8,11 +8,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-const allowedOrigin = process.env.FRONTEND_URL || 'http://localhost:5173';
+const allowedOrigin = process.env.FRONTEND_URL
 
 app.use(cors({
   origin: allowedOrigin,
-  credentials: true, // if you're using cookies or HTTP auth
 }));
 
 app.use(express.json());
