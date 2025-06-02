f620b1f69fa3f25e6957e60dd43a9f71f57d022a
SYN7777777 <redtrikeygg@gmai.com>
2025-06-02 16:32:48 +0530

    changes in serves.js

--- server.js
+++ server.js
@@ -8,18 +8,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+
 const allowedOrigin = process.env.FRONTEND_URL;
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || origin === allowedOrigin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: allowedOrigin,
   credentials: true
 }));
+
 
 
 app.use(express.json());
