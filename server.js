38c0b915b84b578f0dcfbc9c9d632eff711fcca2
SYN7777777 <redtrikeygg@gmai.com>
2025-06-02 16:39:11 +0530

    chnages

--- server.js
+++ server.js
@@ -9,14 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 
-const allowedOrigin = process.env.FRONTEND_URL;
-
-app.use(cors({
-  origin: allowedOrigin,
-  credentials: true
-}));
-
-
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
